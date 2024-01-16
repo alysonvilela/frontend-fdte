5437e7e77d139c9a1bd795752f3e6e3696177ce8
Alyson Vilela <ctt.alysonvilela@gmail.com>
2024-01-16 04:23:22 -0300

    chore: deps

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyled from "./assets/styles/global";
 import { Routes } from "./routes";
 import { theme } from "./assets/styles/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyled />
         <Routes />
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
